ac0d4f9634986480de42a1935c83a81e108e4a3e
Duong Pham Qui <duongpq@nal.vn>
2023-02-17 20:56:37 +0700

    add four years to count

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 var timeout = null;
-const second_of_day = 24*60*60;
-const second_of_hour = 60*60;
+const second_of_day = 24 * 60 * 60;
+const second_of_hour = 60 * 60;
 const second_of_minute = 60;
-
+const NEW_YEAR_DATE = [
+    new Date(2023, 0, 22, 0, 0, 0, 0),
+    new Date(2024, 1, 10, 0, 0, 0, 0),
+    new Date(2025, 0, 29, 0, 0, 0, 0),
+    new Date(2026, 1, 17, 0, 0, 0, 0),
+];
 
 function init() {
-    const event_time = new Date(2023, 0, 22, 0, 0, 0, 0);
-    loop (Math.floor(Math.abs(event_time - Date.now())/1000));
+    const NOW = Date.now();
+
+    for (new_year of NEW_YEAR_DATE)
+        if (new_year > NOW)
+            break;
+
+    document.getElementById('year').innerHTML = new_year.getYear() + 1900;
+
+    loop(Math.floor(Math.abs(new_year - NOW) / 1000));
 }
 
 
 function loop(count) {
     temp = count;
-    
-    day = Math.floor(temp/second_of_day);
-    temp = temp%second_of_day;
 
-    hour = Math.floor(temp/second_of_hour);
-    temp = temp%second_of_hour;
+    day = Math.floor(temp / second_of_day);
+    temp = temp % second_of_day;
 
-    minute = Math.floor(temp/second_of_minute);
-    temp = temp%second_of_minute;
+    hour = Math.floor(temp / second_of_hour);
+    temp = temp % second_of_hour;
+
+    minute = Math.floor(temp / second_of_minute);
+    temp = temp % second_of_minute;
 
     second = Math.floor(temp);
 
@@ -28,12 +40,6 @@ function loop(count) {
     document.getElementById('hour').innerHTML = hour;
     document.getElementById('minute').innerHTML = minute;
     document.getElementById('second').innerHTML = second;
-
-    if (count < 0) {
-        alert('Qua năm mới rồi bạn ơi!');
-        clearTimeout(timeout);
-        return false;
-    }
 
     timeout = setTimeout(function () {
         count -= 1;
